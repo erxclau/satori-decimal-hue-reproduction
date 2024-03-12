76aa3866a9c1fc1ddef7c0c5c9cd83e8b43c7263
Ashwin Mahadevan <mahadevan@ashwinm.com>
2024-03-11 18:56:01 -0700

    Reproduction

---
+++ app/route.jsx
@@ -0,0 +1,20 @@
+import { ImageResponse } from "next/og";
+
+export function GET(request) {
+  const decimal = request.nextUrl.searchParams.get("decimal") === "true";
+
+  const element = (
+    <div
+      style={{
+        display: "flex",
+        width: 256,
+        height: 256,
+        backgroundColor: decimal
+          ? "hsl(20.5 90.2% 48.2%)" // Shows black image with decimal hue.
+          : "hsl(21 90.2% 48.2%)", // Shows correct orange image with integer hue.
+      }}
+    />
+  );
+
+  return new ImageResponse(element, { width: 256, height: 256 });
+}
