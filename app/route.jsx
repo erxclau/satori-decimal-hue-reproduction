3c2b223b7dc9c88fe143ffa06f34f4c221949fd1
erxclau <ericlau41802@gmail.com>
2024-11-29 21:50:25 -0500

    Update next

--- app/route.jsx
+++ app/route.jsx
@@ -12,8 +12,12 @@ export function GET(request) {
         backgroundColor: decimal
           ? "hsl(20.5 90.2% 48.2%)" // Shows black image with decimal hue.
           : "hsl(21 90.2% 48.2%)", // Shows correct orange image with integer hue.
+        color: "white",
+        WebkitTextStroke: "1px red"
       }}
-    />
+    >
+      hello world
+    </div>
   );
 
   return new ImageResponse(element, { width: 256, height: 256 });
